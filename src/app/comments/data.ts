525e94ed94534d6ddbab431204c27f2f4631b2b1
Yogurt7v <egorov-2k@yandex.ru>
2024-06-07 11:28:30 +0300

    URL Query Parameters

---
+++ src/app/comments/data.ts
@@ -0,0 +1,18 @@
+export const comments = [
+    {
+        id: 1,
+        text: "Hello, World!",
+        author: "Me"
+    }, 
+    {
+        id: 2,
+        text: "Hello, ALL!",
+        author: "Everyone"
+    },
+    {
+        id: 3,
+        text: "Hello, Everyone!",
+        author: "My friend"
+    }
+
+]
